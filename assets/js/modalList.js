d0a7de91b51e3d4564dcf5a7a1ce8ff0c8580b7c
fer_nav <fer22navarrete@gmail.com>
2021-06-08 20:13:32 -0300

    inp-type

--- assets/js/modalList.js
+++ assets/js/modalList.js
@@ -1,28 +1,35 @@
 //INICIA EL CLIENTE
-var client = ZAFClient.init()
-document.querySelector('#modal').addEventListener('submit', this.onModalSubmit.bind());
+var client = ZAFClient.init();
+document
+  .querySelector('#modal')
+  .addEventListener('submit', this.onModalSubmit.bind());
 //SUBMIT
 function onModalSubmit() {
-    event.preventDefault()
-    submitData()
-    client.invoke('destroy');
-
+  event.preventDefault();
+  submitData();
+  client.invoke('destroy');
 }
 function submitData() {
-    const resultado = obtenerDatos()
-    localStorage.setItem('ProposedCustomization', JSON.stringify(resultado))
+  const resultado = obtenerDatos();
+  localStorage.setItem('ProposedCustomization', JSON.stringify(resultado));
 }
-function obtenerDatos() {
-    type = document.getElementById("inp-type").value;
-    scriptid = document.getElementById("inp-scriptid").value;
-    let r = []
-    if (scriptid !== '') {
-        r.push(scriptid)
-    }
-    if (type !== '') {
-        r.push(type)
-    }
-    return r;
-}
-
-
+$('#inp-type').change(function () {
+  var prefix = $(this).val();
+  if (prefix != '99999') {
+    document.getElementById('inp-scriptid').value = $(this).val() + '_';
+  } else {
+    document.getElementById('inp-scriptid').value = '';
+  }
+});
+// function obtenerDatos() {
+//     type = document.getElementById("inp-type").value;
+//     scriptid = document.getElementById("inp-scriptid").value;
+//     let r = []
+//     if (scriptid !== '') {
+//         r.push(scriptid)
+//     }
+//     if (type !== '') {
+//         r.push(type)
+//     }
+//     return r;
+// }
